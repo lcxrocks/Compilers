7a59fea3000bb7e1873eb2d1647d88a863ebefaf
181240035-Liu Chunxu <181240035@smail.nju.edu.cn>
2021-06-09 17:25:35 +0800

    before changing init_memory

--- Code/mips.c
+++ Code/mips.c
@@ -23,8 +23,77 @@ void init_write(FILE* fp){
     fprintf(fp, "\n");
 }
 
-void init_mips(FILE* fp){
+// void init_var(FILE* fp, Operand* op){
+//     if(op->op_kind == OP_TEMP){
+//         fprintf(fp, "\tt%d: .word 0\n", op->tmp_no);
+//     }
+//     else if(op->op_kind == OP_VARIABLE){
+//         fprintf(fp, "\tv%d: .word 0\n", op->tmp_no);
+//     }
+//     else{
+//         log("init_var: should not have op_kind:[%d]\n", op->op_kind);
+//         assert(0);
+//     }
+// }
+
+void init_var(FILE* fp, int var_no, int temp_no){
+    for(int i=1; i<=var_no; i++){
+        fprintf(fp, "\tv%d: .word 0\n", i);
+    }
+    for(int i=1; i<=temp_no; i++){
+        fprintf(fp, "\tt%d: .word 0\n", i);
+    }
+}
+
+void init_memory(FILE* fp, InterCodes *start){
+    InterCodes *p = start;
+    while(p){
+        InterCodes *ir = p->code;
+        switch (ir->ir_kind)
+        {
+        case IR_ASSIGN:
+            print_op(fp, ir->lr.op1);
+            fprintf(fp, " := ");
+            print_op(fp, ir->lr.op2);
+            break;
+        case IR_ADD:
+        case IR_SUB:
+        case IR_DIV:
+        case IR_MUL:
+            print_binop(fp, ir);
+            break;
+        case IR_GET_ADDR:
+        case IR_GET_ADDR_VAL:
+        case IR_TO_ADDR:
+            log("init_memory: Address related not implemented.\n");
+            break;
+        case IR_DEC:
+            log("init_memory: DEC not implemented.\n");
+            break;
+        case IR_ARG: 
+            fprintf(fp, "ARG ");
+            // if(ir->unop.op->op_kind==OP_ADDRESS)
+            //     fprintf(fp, "&");
+            print_op(fp, ir->unop.op);
+            break;
+        case IR_CALL_FUNC: 
+            print_op(fp, ir->lr.op1);
+            break;
+        case IR_PARAM:
+            fprintf(fp, "PARAM "); // v
+            print_op(fp, ir->unop.op);
+            break;
+        default:
+            log("one ir not implemented!");
+            break;
+        }
+        p = p->next;
+    }
+}
+
+void init_mips(FILE* fp, InterCodes *start){
     fprintf(fp, ".data\n");
+    init_memory(fp, start);
     fprintf(fp, "_prompt: .asciiz \"Enter an integer:\"\n");
     fprintf(fp, "_ret: .asciiz \"\\n\"\n");
     fprintf(fp, ".global main\n");
@@ -33,11 +102,12 @@ void init_mips(FILE* fp){
     init_write(fp);
 }
 
-void reg(Operand *x){
+char* reg(Operand *x, int reg_no){ // no: {0, 1, 2, 3}
 
 }
 
 void print_mips(FILE* fp, InterCodes *start){
+    init_mips(fp, start);
     InterCodes *p = start;
     while(p!=NULL){
         InterCode *ir = p->code;
@@ -47,7 +117,12 @@ void print_mips(FILE* fp, InterCodes *start){
             fprintf(fp, "label%d:", ir->unop.op->label_no);
             break;
         case IR_ASSIGN:
-            if()
+            if(ir->lr.op2->op_kind == OP_CONSTANT){
+                fprintf(fp, "li %s, %d", reg(ir->lr.op1, 0), ir->lr.op2->const_value);
+            }
+            else{
+                fprintf(fp, "move %s, %s", reg(ir->lr.op1, 0), reg(ir->lr.op2, 1));
+            }
             break;
         
         default:
